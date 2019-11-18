f9371d92df3946a7c09a5ae01cee379ed171a2c6
aidan-lane <aidanlane7@gmail.com>
2019-11-18 14:20:30 -0500

    filter and sort assignments array

--- src/views/assignments/AutoAssignments.vue
+++ src/views/assignments/AutoAssignments.vue
@@ -7,7 +7,7 @@
       >
         <header class="modal-card-head">
           <p class="modal-card-title">
-            Auto-Assign Assignments
+            Auto-Schedule Assignments
           </p>
         </header>
         <section class="modal-card-body">
@@ -31,7 +31,7 @@
                 :key="element.id"
                 class="list-group-item"
               >
-                {{ element.id }}
+                {{ element.title }}
                 <div class="index">
                   {{ index + 1 }}
                 </div>
@@ -42,6 +42,12 @@
         <footer class="modal-card-foot">
           <b-button @click="$parent.close()">
             Cancel
+          </b-button>
+          <b-button
+            type="is-primary"
+            @click="submitAutoAssign()"
+          >
+            Submit
           </b-button>
         </footer>
       </div>
@@ -59,20 +65,7 @@ export default {
   props: {},
   data () {
     return {
-      assignments: [
-        {
-          id: 'Aidan'
-        },
-        {
-          id: 'Caleb'
-        },
-        {
-          id: 'Justin'
-        },
-        {
-          id: 'Ryan'
-        }
-      ],
+      assignments: [],
       drag: false
     }
   },
@@ -86,9 +79,40 @@ export default {
       }
     }
   },
+  mounted () {
+    // get upcoming assignments from assessments
+    this.assignments = this.$store.state.assessments.upcomingAssessments
+      .filter(item => item.assessmentType === 'assignment' && item.fullyScheduled === false)
+      .sort((a, b) => { return b.priority - a.priority })
+  },
   methods: {
-    autoAssign () {
+    submitAutoAssign () {
+      this.autoAssign()
 
+      this.$buefy.toast.open({
+        type: 'is-success',
+        message: 'Auto-scheduled your assignments!'
+      })
+    },
+    autoAssign () {
+      // get furthest assignment due date
+      let furthest = this.assignments.reduce((max, a) =>
+        a.dueDate > max ? a.dueDate : max, this.assignments[0].dueDate)
+
+      // find # of days between now and furthest due date
+      let dayspan = new Date(new Date(furthest) - new Date()).getDate()
+
+      // create a map where each key is the date and value is a map representing 15-min time slots
+      let timeMap = new Map()
+      let curDate = new Date()
+      for (let i = 0; i < dayspan; i++) {
+        let newDateKey = new Date(curDate.getFullYear(), curDate.getMonth(), curDate.getDate() + i, 0, 0, 0, 0)
+        timeMap.set(newDateKey, new Map())
+      }
+
+      // for latest - earliest, add
+
+      console.log(this.$store.state)
     }
   }
 }
@@ -143,15 +167,15 @@ ul {
   text-align: center;
   padding: 3px;
   border-radius: 5px;
-  background-image: linear-gradient(-20deg, #fc6076 0%, #ff9a44 100%);
+  background-image: linear-gradient(-20deg, #fc6076 20%, #ff9a44 100%);
   margin-bottom: 0.5em;
 }
 
 .index {
   float: right;
   border-radius: 50% !important;
-  width: 10%;
-  height: 10%;
+  width: 1.5em;
+  height: 1.5em;
   padding: 0px;
   background-image: linear-gradient(-20deg, #fc6076 0%, #ff9a44 100%);
   color: #ffffff;

e1f4252b0d96f53d8328ebdc27a73108664d8e16
Andrew Smith <smitha24@rpi.edu>
2019-12-20 08:55:51 -0500

    Cleared comments

--- assistant/functions/index.js
+++ assistant/functions/index.js
@@ -10,11 +10,3 @@ app.intent('late_login_yes', (conv) => {
 
 // Set the DialogflowApp object to handle the HTTPS POST request.
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app)
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
